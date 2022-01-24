365906b6b91ab5f0e11db33fa03ff50084da2316
Shubham Aher <ahers247@gmail.com>
2022-01-24 14:05:02 +0530

    js complitd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let marks=0;
+function submit(){
+    if(document.getElementById("op14").checked)marks=marks+1;
+    if(document.getElementById("op22").checked)marks=marks+1;
+    if(document.getElementById("op33").checked)marks=marks+1;
+    const ma= document.getElementById("ans-marks").innerHTML="Score: "+marks;
+  
 
+    const main = document.getElementById("main")
+    main.remove();
+}
